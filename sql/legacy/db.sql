5b9903cb1eded4780df153453545d94dddb5eda8
Ozum Eldogan <ozum@ozum.net>
2016-01-26 15:53:00 +0200

    Doc Update

--- sql/legacy/db.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    catalog_name AS "name",
-    catalog_name AS "fullName",
-    catalog_name AS "fullCatalogName"
-FROM
-    information_schema.information_schema_catalog_name
-ORDER BY
-    catalog_name
