1f07f465f465f5e1cf75dd05b148ac19a6ae9706
Özüm Eldoğan <ozum@ozum.net>
2020-04-16 13:43:18 +0300

    fix: update dependencies

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,16 @@
 const packageData = require("../../package.json");
 const getVuePressBars = require("vuepress-bar");
 const { nav, sidebar } = getVuePressBars(__dirname + "/..");
+const vuepressConfig = require("../../.devkeeperrc.json").vuepress || {};
+
+const plugins = ["@vuepress/active-header-links", "@vuepress/pwa"];
+
+if (vuepressConfig.googleAnalytics && vuepressConfig.googleAnalytics.id) plugins.push(["@vuepress/google-analytics", { ga: vuepressConfig.googleAnalytics.id }])
 
 module.exports = {
   title: packageData.label || packageData.name,
   description: packageData.description,
-  plugins: ["@vuepress/active-header-links", "@vuepress/plugin-pwa"],
+  plugins,
   themeConfig: {
     repo: typeof packageData.repository === "string" ? packageData.repository : packageData.repository.url,
     nav,
