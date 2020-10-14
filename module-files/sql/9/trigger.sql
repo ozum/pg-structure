92c1a9b108acc9818266855ab5e81b79dc369e3b
Mark Doeswijk <mark@springtree.nl>
2020-10-14 09:11:54 +0200

    fix(sql): there is no regexp_match on pg9

--- module-files/sql/9/trigger.sql
+++ module-files/sql/9/trigger.sql
@@ -20,7 +20,7 @@ SELECT
     (CASE WHEN (tgtype::int::bit(7) & b'0100000')::int = 0 THEN '' ELSE ' truncate' END)
   ), ' ')                                                                                   AS "events", -- Array, Ex:  {insert,delete,update}
 
-  (regexp_match(pg_get_triggerdef(t.oid), '.{35,} WHEN \((.+)\) EXECUTE FUNCTION'))[1]      AS "condition",
+  (regexp_matches(pg_get_triggerdef(t.oid), '.{35,} WHEN \((.+)\) EXECUTE FUNCTION'))[1]      AS "condition",
 
   CASE tgenabled
     WHEN 'O' THEN 'origin'
