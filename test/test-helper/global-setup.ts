95d80b8d3685bf52414d450e077364c2a6125a0d
Özüm Eldoğan <ozum@ozum.net>
2021-01-27 14:42:51 +0300

    test: fix tests succeeding without pg server

--- test/test-helper/global-setup.ts
+++ test/test-helper/global-setup.ts
@@ -2,25 +2,14 @@ import PgTestUtil from "pg-test-util";
 
 module.exports = async () => {
   const pgTestUtil = new PgTestUtil({ connection: { connectionString: "postgresql://user:password@127.0.0.1:5432/template1" } });
-  try {
-    await pgTestUtil.createDatabase({
-      drop: true,
-      name: "pg-structure-test-main",
-      file: `${__dirname}/ddl/main.sql`,
-    });
-    await pgTestUtil.createDatabase({
-      drop: true,
-      name: "pg-structure-test-relation-names",
-      file: `${__dirname}/ddl/relation-names.sql`,
-    });
-    await pgTestUtil.createDatabase({
-      drop: true,
-      name: "pg-structure-test-relation-names-reverse",
-      file: `${__dirname}/ddl/relation-names-reverse.sql`,
-    });
-  } catch (e) {
-    console.log(e); // eslint-disable-line no-console
-  }
+
+  await pgTestUtil.createDatabase({ drop: true, name: "pg-structure-test-main", file: `${__dirname}/ddl/main.sql` });
+  await pgTestUtil.createDatabase({ drop: true, name: "pg-structure-test-relation-names", file: `${__dirname}/ddl/relation-names.sql` });
+  await pgTestUtil.createDatabase({
+    drop: true,
+    name: "pg-structure-test-relation-names-reverse",
+    file: `${__dirname}/ddl/relation-names-reverse.sql`,
+  });
 
   (global as any).pgTestUtil = pgTestUtil;
 };
