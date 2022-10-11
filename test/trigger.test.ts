f4c2ed97fd27a7f1dfd0e2e3720c42de780f2988
David Marcin <david@metawork.com>
2022-10-10 17:20:38 -0700

    fix(triggers): fix introspection on triggers without WHEN clauses
    Triggers without WHEN clauses were being erroneously discarded. See #78 for details.
    
    fix #78

--- test/trigger.test.ts
+++ test/trigger.test.ts
@@ -2,39 +2,47 @@ import { Db, Trigger } from "../src/index";
 import getDb from "./test-helper/get-db";
 
 let db: Db;
-let trigger: Trigger;
+let accountTrigger: Trigger;
 
 beforeAll(async () => {
   db = await getDb();
-  trigger = db.schemas.get("public").tables.get("account").triggers.get("account_trigger");
+  accountTrigger = db.schemas.get("public").tables.get("account").triggers.get("account_trigger");
 });
 
 describe("Trigger", () => {
+  it("should exist.", () => {
+    // Test that we can find a trigger with and without the WHEN clause.
+    const withWhen = db.schemas.get("public").tables.get("account").triggers.get("account_trigger", { throwUnknown: false });
+    expect(withWhen).not.toBeUndefined();
+    const withoutWhen = db.schemas.get("public").tables.get("account").triggers.get("account_updated_at", { throwUnknown: false });
+    expect(withoutWhen).not.toBeUndefined();
+  });
+
   it("should have name.", () => {
-    expect(trigger.name).toBe("account_trigger");
+    expect(accountTrigger.name).toBe("account_trigger");
   });
 
   it("should have table.", () => {
-    expect(trigger?.table?.name).toBe("account");
+    expect(accountTrigger?.table?.name).toBe("account");
   });
 
   it("should have view.", () => {
-    expect(trigger?.view?.name).toBeUndefined();
+    expect(accountTrigger?.view?.name).toBeUndefined();
   });
 
   it("should have schema.", () => {
-    expect(trigger.schema.name).toBe("public");
+    expect(accountTrigger.schema.name).toBe("public");
   });
 
   it("should have full name.", () => {
-    expect(trigger.fullName).toBe("public.account.account_trigger");
+    expect(accountTrigger.fullName).toBe("public.account.account_trigger");
   });
 
   it("should have events.", () => {
-    expect(trigger.events).toEqual(["insert", "delete", "update"]);
+    expect(accountTrigger.events).toEqual(["insert", "delete", "update"]);
   });
 
   it("should have function.", () => {
-    expect(trigger.function.name).toBe("trigger_returning_function");
+    expect(accountTrigger.function.name).toBe("trigger_returning_function");
   });
 });
